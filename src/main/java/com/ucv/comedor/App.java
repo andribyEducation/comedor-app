82bdc97b206f21cfc2836bfb26803f973162cb53
CarloZ18 <carlosgmatal@gmail.com>
2025-07-16 21:36:54 -0400

    Add Validation Admin

--- src/main/java/com/ucv/comedor/App.java
+++ src/main/java/com/ucv/comedor/App.java
@@ -3,15 +3,21 @@ package com.ucv.comedor;
 import javax.swing.SwingUtilities;
 import views.home.Home;
 import controllers.HomeController;
+import views.admin.ValidationAdminView;
 
 public class App 
 {
     public static void main( String[] args )
     {
          SwingUtilities.invokeLater(() -> {
-            Home homeView = new Home();
+            /*Home homeView = new Home();
             new HomeController(homeView);
-            homeView.setVisible(true);
+            homeView.setVisible(true);*/
+
+            ValidationAdminView validationAdminView = new ValidationAdminView();
+            validationAdminView.setVisible(true);
+
+            
         });
     }
 }

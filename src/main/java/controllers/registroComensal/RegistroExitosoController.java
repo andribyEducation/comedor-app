fd1f3600ae1f34c5f5093dca83793a7f0dae3825
Adrian <adrian.dryzara3@gmail.com>
2025-07-18 01:29:03 -0400

    Registro exitoso

---
+++ src/main/java/controllers/registroComensal/RegistroExitosoController.java
@@ -0,0 +1,24 @@
+package controllers.registroComensal;
+
+import views.registroComensalView.RegistroExitosoView;
+import views.login.LoginView;
+import controllers.LoginController;
+
+public class RegistroExitosoController {
+
+    private RegistroExitosoView view;
+
+    public RegistroExitosoController(RegistroExitosoView view) {
+        this.view = view;
+        initListeners();
+    }
+
+    private void initListeners() {
+        view.getIngresarButton().addActionListener(e -> {
+            view.setVisible(false);
+            LoginView loginView = new LoginView();
+            new LoginController(loginView);
+            loginView.setVisible(true);
+        });
+    }
+}

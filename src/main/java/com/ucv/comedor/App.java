3f6c246a30b8088e1a2cd8c18b7cfa2f8227b9da
Adrian <adrian.dryzara3@gmail.com>
2025-07-17 21:29:25 -0400

    Merge conflicts

--- src/main/java/com/ucv/comedor/App.java
+++ src/main/java/com/ucv/comedor/App.java
@@ -13,11 +13,24 @@ public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
 <<<<<<< HEAD
+<<<<<<< HEAD
             Home view = new Home();
             // new AdminDashboardController(view);
 =======
             RegistroComensalView view = new RegistroComensalView();
 >>>>>>> 64616df (registro general)
+=======
+<<<<<<< HEAD
+            RegistroComensalView view = new RegistroComensalView();
+=======
+            AdminDashboardView view = new AdminDashboardView();
+            new AdminDashboardController(view);
+>>>>>>> 94619347654f87e930bfbf360b35178a9ad96adc
+=======
+            Home view = new Home();
+            // new AdminDashboardController(view);
+>>>>>>> 97cc32c (Merge conflicts)
+>>>>>>> 40b99bc (Merge conflicts)
             view.setVisible(true);
         });
     }

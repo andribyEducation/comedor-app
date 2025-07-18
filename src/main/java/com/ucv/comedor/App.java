24b0d571fe49864af7ffb0c3f630f0d17ae9c048
Adrian <adrian.dryzara3@gmail.com>
2025-07-17 21:48:25 -0400

    Merge

--- src/main/java/com/ucv/comedor/App.java
+++ src/main/java/com/ucv/comedor/App.java
@@ -1,5 +1,12 @@
 package com.ucv.comedor;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+import views.Registro_Comensal_view.RegistroComensalView;
+
+>>>>>>> 197ea30 (Merge)
 =======
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -16,6 +23,8 @@ import controllers.AdminDashboardController;
 =======
 >>>>>>> 94619347654f87e930bfbf360b35178a9ad96adc
 =======
+=======
+>>>>>>> 885072b (Merge)
 import views.admin.dashboards.AdminDashboardView;
 import views.home.Home;
 import controllers.AdminDashboardController;
@@ -37,13 +46,26 @@ import views.home.Home;
 import controllers.AdminDashboardController;
 >>>>>>> 94619347654f87e930bfbf360b35178a9ad96adc
 >>>>>>> 3393218 (Merge)
+<<<<<<< HEAD
 >>>>>>> d78481e (Merge)
+<<<<<<< HEAD
 >>>>>>> 2d91c84 (Merge)
+=======
+=======
+=======
+import views.Registro_Comensal_view.RegistroComensalView;
+import views.admin.dashboards.AdminDashboardView;
+import views.home.Home;
+import controllers.AdminDashboardController;
+>>>>>>> d47aa93 (App.java)
+>>>>>>> 885072b (Merge)
+>>>>>>> 197ea30 (Merge)
 import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -58,6 +80,8 @@ public class App {
 >>>>>>> b4f20e4 (Merge)
 =======
 >>>>>>> 2d91c84 (Merge)
+=======
+>>>>>>> 197ea30 (Merge)
 <<<<<<< HEAD
             RegistroComensalView view = new RegistroComensalView();
 =======
@@ -74,6 +98,8 @@ public class App {
 =======
 =======
 >>>>>>> d78481e (Merge)
+=======
+>>>>>>> 885072b (Merge)
             Home view = new Home();
             // new AdminDashboardController(view);
 =======
@@ -97,8 +123,18 @@ public class App {
             // new AdminDashboardController(view);
 >>>>>>> 97cc32c (Merge conflicts)
 >>>>>>> 3393218 (Merge)
+<<<<<<< HEAD
 >>>>>>> d78481e (Merge)
+<<<<<<< HEAD
 >>>>>>> 2d91c84 (Merge)
+=======
+=======
+=======
+            Home view = new Home();
+            // new AdminDashboardController(view);
+>>>>>>> d47aa93 (App.java)
+>>>>>>> 885072b (Merge)
+>>>>>>> 197ea30 (Merge)
             view.setVisible(true);
         });
     }

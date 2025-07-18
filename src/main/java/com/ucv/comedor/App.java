fda6cbb76ca46b05ff350a96ef720fef7bedb240
Adrian <adrian.dryzara3@gmail.com>
2025-07-17 21:50:25 -0400

    app

--- src/main/java/com/ucv/comedor/App.java
+++ src/main/java/com/ucv/comedor/App.java
@@ -1,56 +1,15 @@
 package com.ucv.comedor;
-<<<<<<< HEAD
-<<<<<<< HEAD
-import views.admin.dashboards.AdminDashboardView;
-import views.home.Home;
-import controllers.AdminDashboardController;
-=======
-import views.Registro_Comensal_view.RegistroComensalView;
-
-<<<<<<< HEAD
->>>>>>> 64616df (registro general)
-=======
-=======
-import views.admin.dashboards.AdminDashboardView;
-import views.home.Home;
-import controllers.AdminDashboardController;
->>>>>>> 94619347654f87e930bfbf360b35178a9ad96adc
->>>>>>> 3393218 (Merge)
-=======
 import views.Registro_Comensal_view.RegistroComensalView;
 import views.admin.dashboards.AdminDashboardView;
 import views.home.Home;
 import controllers.AdminDashboardController;
->>>>>>> d47aa93 (App.java)
 import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             Home view = new Home();
             // new AdminDashboardController(view);
-=======
-            RegistroComensalView view = new RegistroComensalView();
->>>>>>> 64616df (registro general)
-=======
-<<<<<<< HEAD
-            RegistroComensalView view = new RegistroComensalView();
-=======
-            AdminDashboardView view = new AdminDashboardView();
-            new AdminDashboardController(view);
->>>>>>> 94619347654f87e930bfbf360b35178a9ad96adc
-=======
-            Home view = new Home();
-            // new AdminDashboardController(view);
->>>>>>> 97cc32c (Merge conflicts)
->>>>>>> 3393218 (Merge)
-=======
-            Home view = new Home();
-            // new AdminDashboardController(view);
->>>>>>> d47aa93 (App.java)
             view.setVisible(true);
         });
     }

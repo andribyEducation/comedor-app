2499a7078e67975dde3cfdb6521c109fb8c7e438
Adrian <adrian.dryzara3@gmail.com>
2025-07-17 21:48:06 -0400

    Merge

--- src/main/java/com/ucv/comedor/App.java
+++ src/main/java/com/ucv/comedor/App.java
@@ -22,14 +22,29 @@ import controllers.AdminDashboardController;
 =======
 import views.Registro_Comensal_view.RegistroComensalView;
 
+<<<<<<< HEAD
 >>>>>>> 64616df (registro general)
+<<<<<<< HEAD
 >>>>>>> 395627d (Merge)
+<<<<<<< HEAD
 >>>>>>> b4f20e4 (Merge)
+=======
+=======
+=======
+=======
+import views.admin.dashboards.AdminDashboardView;
+import views.home.Home;
+import controllers.AdminDashboardController;
+>>>>>>> 94619347654f87e930bfbf360b35178a9ad96adc
+>>>>>>> 3393218 (Merge)
+>>>>>>> d78481e (Merge)
+>>>>>>> 2d91c84 (Merge)
 import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -41,6 +56,8 @@ public class App {
 =======
 =======
 >>>>>>> b4f20e4 (Merge)
+=======
+>>>>>>> 2d91c84 (Merge)
 <<<<<<< HEAD
             RegistroComensalView view = new RegistroComensalView();
 =======
@@ -55,13 +72,33 @@ public class App {
 >>>>>>> 40b99bc (Merge conflicts)
 =======
 =======
+=======
+>>>>>>> d78481e (Merge)
             Home view = new Home();
             // new AdminDashboardController(view);
 =======
             RegistroComensalView view = new RegistroComensalView();
 >>>>>>> 64616df (registro general)
+<<<<<<< HEAD
 >>>>>>> 395627d (Merge)
+<<<<<<< HEAD
 >>>>>>> b4f20e4 (Merge)
+=======
+=======
+=======
+<<<<<<< HEAD
+            RegistroComensalView view = new RegistroComensalView();
+=======
+            AdminDashboardView view = new AdminDashboardView();
+            new AdminDashboardController(view);
+>>>>>>> 94619347654f87e930bfbf360b35178a9ad96adc
+=======
+            Home view = new Home();
+            // new AdminDashboardController(view);
+>>>>>>> 97cc32c (Merge conflicts)
+>>>>>>> 3393218 (Merge)
+>>>>>>> d78481e (Merge)
+>>>>>>> 2d91c84 (Merge)
             view.setVisible(true);
         });
     }

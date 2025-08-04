55b5f54ed0428551b0eff9dfeb45ecdbb5d32b6d
CarloZ18 <carlosgmatal@gmail.com>
2025-08-03 23:06:16 -0400

    Fix Admin

--- src/main/java/com/ucv/comedor/App.java
+++ src/main/java/com/ucv/comedor/App.java
@@ -1,5 +1,7 @@
 package com.ucv.comedor;
+import com.ucv.views.admin.dashboards.AdminDashboardView;
 import com.ucv.views.home.Home;
+import com.ucv.controllers.adminDashboard.AdminDashboardController;
 import com.ucv.controllers.home.HomeController;
 import javax.swing.SwingUtilities;
 
@@ -7,9 +9,13 @@ public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
 
-            Home view = new Home();
+            /*Home view = new Home();
             new HomeController(view);
-            view.setVisible(true);
+            view.setVisible(true); */
+
+            AdminDashboardView adminDashboardView = new AdminDashboardView();
+            new AdminDashboardController(adminDashboardView);
+            adminDashboardView.setVisible(true);
         });
     }
 }
